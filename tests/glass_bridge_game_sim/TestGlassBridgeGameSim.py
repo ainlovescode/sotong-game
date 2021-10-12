db0c126ec75e6d62a9b11ce36af81f2f23b94334
Ainul-MdRazib <hi@ainlovescode.com>
2021-10-13 03:33:18 +0800

    [ain] Add player memory to account for step memorisation

--- tests/glass_bridge_game_sim/TestGlassBridgeGameSim.py
+++ tests/glass_bridge_game_sim/TestGlassBridgeGameSim.py
@@ -44,13 +44,32 @@ class TestGlassBridgeGameSim(unittest.TestCase):
     def test_print_player_survival(self, mock_stdout):
         glass_bridge_game_sim = GlassBridgeGameSim(num_of_itr=5)
         glass_bridge_game_sim.survivals = [4, 2, 3]
-        expected_output = "Player 1 has a 80.0% chance of survival.\n" \
-                          "Player 2 has a 40.0% chance of survival.\n" \
-                          "Player 3 has a 60.0% chance of survival.\n"
+        expected_player_1_output = "Player 1 has a 80.0% chance of survival."
+        expected_player_2_output = "Player 2 has a 40.0% chance of survival."
+        expected_player_3_output = "Player 3 has a 60.0% chance of survival."
 
         glass_bridge_game_sim.print_survival_chances()
+        mock_stdout_value = mock_stdout.getvalue()
 
-        self.assertEqual(mock_stdout.getvalue(), expected_output)
+        self.assertIn(expected_player_1_output, mock_stdout_value)
+        self.assertIn(expected_player_2_output, mock_stdout_value)
+        self.assertIn(expected_player_3_output, mock_stdout_value)
+
+    @mock.patch('sotonggame.glass_bridge_game_sim.GlassBridgeGameSim.GlassBridgeGameSim.break_panel')
+    def test_player_forgets_when_nervous(self, mock_break_panel):
+        t_player_memory = 2
+        t_panels = [0] * 3
+
+        glass_bridge_game_sim = GlassBridgeGameSim(num_of_players=1,
+                                                   num_of_steps=3,
+                                                   num_of_itr=1,
+                                                   player_memory=t_player_memory)
+
+        glass_bridge_game_sim.panels = t_panels
+
+        glass_bridge_game_sim.run_sim()
+
+        mock_break_panel.assert_called()
 
 
 if __name__ == '__main__':
